6ef3c4799102e088301816e9a66850a8eefe01ef
Grant Pierce <glpierce174@gmail.com>
2022-06-02 14:12:46 -0700

    Improved project onClick behavior for UX.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,15 @@ import CardContent from '@mui/material/CardContent';
 import iCharge from "../images/iCharge.png"
 import ForeverPin from "../images/ForeverPin.png"
 import GitHubMark from "../images/GitHubMark.png"
+import GitHubMarkLarge from "../images/GitHubMarkLarge.png"
 
 function Projects() {
+
+    function linkToGitHub(e, link) {
+        window.open(link, '_blank')
+        e.stopPropagation()
+    }
+
     return(
         <div className="projectsContainer">
             
@@ -15,27 +22,27 @@ function Projects() {
 
             <div className="Center">
                 <div className="projectsRow">
-                    <Card variant="outlined" style={{backgroundColor: "#34425A", maxWidth: "500px"}} className="projectCard">
+                    <Card variant="outlined" style={{backgroundColor: "#34425A", maxWidth: "500px", cursor:'pointer'}} className="projectCard" onClick={e => window.open("https://forever-pin.herokuapp.com/", '_blank')}>
                         <div className="projectImageDiv" style={{backgroundColor: "white"}}>
                             <img src={ForeverPin} className="projectImage" alt="ForeverPin"/>
                         </div> 
                         <CardContent>
                             <div className="projectTitleDiv">
-                                <h2 className="Position" style={{cursor: "pointer"}} onClick={e => window.open("https://forever-pin.herokuapp.com/", '_blank')}>ForeverPin</h2>
-                                <img src={GitHubMark} style={{cursor:'pointer'}} onClick={e => window.open("https://github.com/glpierce/ForeverPin", '_blank')} alt="GitHub Mark"/>
+                                <h2 className="Position">ForeverPin</h2>
+                                <img src={GitHubMarkLarge} style={{cursor:'pointer', height: "34px"}} onClick={e => linkToGitHub(e, "https://github.com/glpierce/ForeverPin")} alt="GitHub Mark"/>
                             </div>
                             <p className="projectDescription">Map journal application with search and filtering tools that enables users to record their travels and connect with friends</p>
                         </CardContent>
                     </Card>
 
-                    <Card variant="outlined" style={{backgroundColor: "#34425A", width: "500px"}} className="projectCard">
+                    <Card variant="outlined" style={{backgroundColor: "#34425A", width: "500px", cursor: "pointer"}} className="projectCard" onClick={e => window.open("https://i-charge.herokuapp.com/", '_blank')}>
                         <div className="projectImageDiv" style={{backgroundColor: "#083C5A"}}>
                             <img src={iCharge} className="projectImage" alt="iCharge"/>
                         </div>
                         <CardContent>
                             <div className="projectTitleDiv">
-                                <h2 className="Position" style={{cursor: "pointer"}} onClick={e => window.open("https://i-charge.herokuapp.com/", '_blank')}>iCharge</h2>
-                                <img src={GitHubMark} style={{cursor:'pointer'}} onClick={e => window.open("https://github.com/glpierce/icharge", '_blank')} alt="GitHub Mark"/>
+                                <h2 className="Position">iCharge</h2>
+                                <img src={GitHubMarkLarge} style={{cursor:'pointer', height: "34px"}} onClick={e => linkToGitHub(e, "https://github.com/glpierce/icharge")} alt="GitHub Mark"/>
                             </div>
                             <p className="projectDescription">Electric vehicle (EV) charging station finder with search and filtering tools that enable users to find and get directions to EV chargers</p>
                         </CardContent>
