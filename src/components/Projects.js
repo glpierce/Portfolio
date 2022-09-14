517656d59ad0e57d47eccd9d4f85c7519cf4111f
Grant Pierce <glpierce174@gmail.com>
2022-09-14 15:13:04 -0700

    Added graph it logo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import Slide from '@mui/material/Slide';
 import iCharge from "../images/iCharge.png"
 import ForeverPin from "../images/ForeverPin.png"
+import GraphIt from "../images/GraphItLogo.png"
 import GitHubMarkLarge from "../images/GitHubMarkLarge.png"
 import GitHubLogo from "../images/GitHubLogo.png"
 
@@ -43,7 +44,27 @@ function Projects() {
                             <p className="projectDescription">Map journal application with search and filtering tools that enables users to record their travels and connect with friends</p>
                         </CardContent>
                     </Card>
-
+                    <Card variant="outlined" style={{backgroundColor: "#34425A", maxWidth: "500px", cursor:'pointer', borderColor: (project === "graph-it" ? "white" : null)}} className="projectCard" onMouseEnter={e => setProject("graph-it")} onMouseLeave={e => setProject(null)} onClick={e => window.open("https://graph-it-react.herokuapp.com/", '_blank')}>
+                        <div className="projectImageDiv" style={{backgroundColor: "white"}}>
+                            <img src={GraphIt} className="projectImage" style={{width: 450}} alt="Graph It"/>
+                        </div> 
+                        <CardContent>
+                            <div className="projectTitleDiv">
+                                <h2 className="Position">Graph It</h2>
+                                <div className="dynamicGitHub" onMouseEnter={e => setGitHub("graph-it")} onMouseLeave={e => setGitHub(null)} onClick={e => linkToGitHub(e, "https://github.com/glpierce/graph-it")}>
+                                    <img src={GitHubMarkLarge} style={{height: "34px"}} alt="GitHub Mark"/>
+                                    <Slide direction="left" in={gitHub==="graph-it" ? true : false} mountOnEnter unmountOnExit>
+                                        <img src={GitHubLogo} alt="GitHub" style={{height: "34px"}}/>
+                                    </Slide>
+                                </div>
+                            </div>
+                            <p className="projectDescription">Directed graph creation application that uses Dijkstra’s algorithm to enable users to find the shortest path between nodes.</p>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+            <div className="Center">
+                <div className="projectsRow">
                     <Card variant="outlined" style={{backgroundColor: "#34425A", width: "500px", cursor: "pointer", borderColor: (project === "icharge" ? "white" : null)}} className="projectCard" onMouseEnter={e => setProject("icharge")} onMouseLeave={e => setProject(null)} onClick={e => window.open("https://i-charge.herokuapp.com/", '_blank')}>
                         <div className="projectImageDiv" style={{backgroundColor: "#083C5A"}}>
                             <img src={iCharge} className="projectImage" alt="iCharge"/>
@@ -59,27 +80,6 @@ function Projects() {
                                 </div>
                             </div>
                             <p className="projectDescription">Electric vehicle (EV) charging station finder with search and filtering tools that enable users to find and get directions to EV chargers</p>
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
-            <div className="Center">
-                <div className="projectsRow">
-                    <Card variant="outlined" style={{backgroundColor: "#34425A", maxWidth: "500px", cursor:'pointer', borderColor: (project === "graph-it" ? "white" : null)}} className="projectCard" onMouseEnter={e => setProject("graph-it")} onMouseLeave={e => setProject(null)} onClick={e => window.open("https://graph-it-react.herokuapp.com/", '_blank')}>
-                        <div className="projectImageDiv" style={{backgroundColor: "white"}}>
-                            <img src={ForeverPin} className="projectImage" alt="Graph It"/>
-                        </div> 
-                        <CardContent>
-                            <div className="projectTitleDiv">
-                                <h2 className="Position">Graph It</h2>
-                                <div className="dynamicGitHub" onMouseEnter={e => setGitHub("graph-it")} onMouseLeave={e => setGitHub(null)} onClick={e => linkToGitHub(e, "https://github.com/glpierce/graph-it")}>
-                                    <img src={GitHubMarkLarge} style={{height: "34px"}} alt="GitHub Mark"/>
-                                    <Slide direction="left" in={gitHub==="graph-it" ? true : false} mountOnEnter unmountOnExit>
-                                        <img src={GitHubLogo} alt="GitHub" style={{height: "34px"}}/>
-                                    </Slide>
-                                </div>
-                            </div>
-                            <p className="projectDescription">Directed graph creation application that uses Dijkstra’s algorithm to enable users to find the shortest path between nodes.</p>
                         </CardContent>
                     </Card>
                 </div>
